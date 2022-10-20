f200802fe88c8b502cb59ea9689627404bb5480f
Emillyvittoria <116230397+Emillyvittoria@users.noreply.github.com>
2022-10-20 01:58:11 +0000

    Meu primeiro projeto Python

--- main.py
+++ main.py
@@ -3,8 +3,20 @@
 # print () = comando de saída
 print("alo mundo!")
 
-''' 
-comentario em bloco
+# Quando quiser guardar uma string (frase)
+nome = "Emilly Vittória"
+# Quando quiser guardar um número inteiro
+idade = 18
+# Exibir o meu nome (o que está dentro da variável nome)
+print (nome) 
 
+# Quando quiser exibir minha idade
+print (idade)
+#print ("Meu nome é" +nome)
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {} anos". format(idade))
 
-'''
+#Quando quiser exibir "Meu nome é ... e tenho ...
+#anos..." trocando pelas variáveos nome e idade
+print("Meu nome é {} e tenho {} anos". format (nome, idade))
