f8b1ae81c40b087ed501f8e7f49d80af6748bf10
Emillyvittoria <116230397+Emillyvittoria@users.noreply.github.com>
2022-11-17 23:24:47 +0000

    Agora nós podemos incluir quaisquer heróis/vilões que o usuário quiser digitar!

--- aula4.2022-11-16b.py
+++ aula4.2022-11-16b.py
@@ -8,10 +8,10 @@ conexao = sqlite3.connect("dc_universe.db")
 cursor = conexao.cursor()
 
 #4º passo: Comando para inserir herói/vilão
-sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')")
+sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')"
 
 #5º passo: Executar o comando SQL no SQLlite (no cursor)
-cursor.execute(sql)
+(cursor.execute(sql)
 
 #6º passo: Confirmar o INSERT com commit() e fechar o banco
 conexao.commit()
